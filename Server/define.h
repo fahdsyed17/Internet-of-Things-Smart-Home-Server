51d0da92184bf6e9a236d348d6d3a50bc83bb384
Fahd Syed <31628747+fahdsyed17@users.noreply.github.com>
2017-09-05 07:27:03 +0400

    Add files via upload

---
+++ Server/define.h
@@ -0,0 +1,17 @@
+
+#ifndef DEFINE_H
+#define	DEFINE_H
+
+  struct table{
+        char string[128];
+        char read[32];
+        char write[32];
+        int size;
+    }table[64];
+    
+    struct {
+        int size;
+    }table_s;
+
+#endif	/* DEFINE_H */
+
